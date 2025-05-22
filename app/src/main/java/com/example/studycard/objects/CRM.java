8686629f611ffb8922ebf86d10c6c21af72b5007
Igor <perekopnyyigor@gmail.com>
2025-05-22 12:20:50 +0300

    work

---
+++ app/src/main/java/com/example/studycard/objects/CRM.java
@@ -0,0 +1,13 @@
+package com.example.studycard.objects;
+
+public class CRM {
+    public static int firstTime=1;
+    public static int openReg=2;
+    public static int pushReg=3;
+    public static int openCours=4;
+    public static int openTopic=5;
+    public static int pressTest=6;
+    public static int doneTest=7;
+    public static int payForm=6;
+    public static int pay=7;
+}
